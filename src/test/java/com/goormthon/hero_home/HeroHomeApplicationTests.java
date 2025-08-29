fe701e8a0898e85f4a72033250959004be982a0d
jihoo2002 <yolohoo@naver.com>
2025-08-29 17:39:19 +0900

    project setting

---
+++ src/test/java/com/goormthon/hero_home/HeroHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormthon.hero_home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
