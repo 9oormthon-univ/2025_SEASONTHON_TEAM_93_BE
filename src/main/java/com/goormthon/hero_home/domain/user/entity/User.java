81c53acd0dba9c232d0239b719a4b2dc31dc81d3
Hwang-sebin <sbsimon0701@gmail.com>
2025-09-05 06:08:04 +0900

    Feat(#8) : User 엔티티에 카카오 소셜 로그인 필드 추가 및 도메인 수정

--- src/main/java/com/goormthon/hero_home/domain/user/entity/User.java
+++ src/main/java/com/goormthon/hero_home/domain/user/entity/User.java
@@ -3,6 +3,7 @@ package com.goormthon.hero_home.domain.user.entity;
 import com.goormthon.hero_home.domain.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -22,6 +23,24 @@ public class User extends BaseEntity {
 
     private String email;
 
+    private String phone_num;
+
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    private String socialId;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Builder
+    public User(String name, String address, String email, String phone_num, Role role, String socialId, SocialType socialType) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.phone_num = phone_num;
+        this.role = role;
+        this.socialId = socialId;
+        this.socialType = socialType;
+    }
 }
