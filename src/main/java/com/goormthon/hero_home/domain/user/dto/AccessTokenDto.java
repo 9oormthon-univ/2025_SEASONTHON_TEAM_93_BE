f07cb3274faaf74b3c600d7dfbbf41d95ae288be
Hwang-sebin <sbsimon0701@gmail.com>
2025-09-05 06:24:16 +0900

    Feat(#8) : 카카오 로그인 DTO AccessToken

---
+++ src/main/java/com/goormthon/hero_home/domain/user/dto/AccessTokenDto.java
@@ -0,0 +1,19 @@
+package com.goormthon.hero_home.domain.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AccessTokenDto {
+    private String access_token;
+    private String token_type;
+    private String refresh_token;
+    private Integer expires_in;
+    private String scope;
+    private Integer refresh_token_expires_in;
+}
