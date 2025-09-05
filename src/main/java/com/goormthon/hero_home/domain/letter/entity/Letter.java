9fa8a61099439a18db0cd8b43d4e7f1a7a605676
Hwang-sebin <sbsimon0701@gmail.com>
2025-09-06 04:40:21 +0900

    Feat(#12): Letter 엔티티 구현

--- src/main/java/com/goormthon/hero_home/domain/letter/entity/Letter.java
+++ src/main/java/com/goormthon/hero_home/domain/letter/entity/Letter.java
@@ -1,9 +1,11 @@
 package com.goormthon.hero_home.domain.letter.entity;
 
+import com.goormthon.hero_home.domain.common.BaseEntity;
 import com.goormthon.hero_home.domain.user.entity.User;
 import com.goormthon.hero_home.domain.warmemoir.entity.WarMemoir;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,23 +13,48 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "letter")
-public class Letter {
+public class Letter extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 200)
     private String title;
 
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
-    private Boolean isCompleted;
+    @Column(nullable = false)
+    private Boolean isCompleted = false;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "war_memoir_id")
     private WarMemoir warMemoir;
+
+    @Builder
+    public Letter(String title, String content, User user, WarMemoir warMemoir) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+        this.warMemoir = warMemoir;
+        this.isCompleted = false;
+    }
+
+    public void updateContent(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void toggleCompleted() {
+        this.isCompleted = !this.isCompleted;
+    }
+
+    public boolean isAuthor(User user) {
+        return this.user.equals(user);
+    }
 }
